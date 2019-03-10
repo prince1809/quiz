13260cef7dcc58795313f82460e40bcb3dfd5531
prince <prince18pat@gmail.com>
2019-03-10 22:54:07 +0900

    initial commit

---
+++ Quiz/ViewController.swift
@@ -0,0 +1,45 @@
+//
+//  ViewController.swift
+//  Quiz
+//
+//  Created by Prince Kumar on 2019/03/10.
+//  Copyright © 2019 Prince Kumar. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    @IBOutlet var questionLabel: UILabel!
+    @IBOutlet var answerLabel: UILabel!
+    
+    let questions: [String] = [
+        "what is 7+7?",
+        "what is the capital of Vermont?",
+        "What is cogmac made from?"
+    ]
+    
+    let answers: [String] = [
+        "14",
+        "Montpelier",
+        "Grapes"
+    ]
+    
+    var currentQuestionIndex: Int = 0
+    
+    @IBAction func showNextQuestion(_ sender: UIButton) {
+        currentQuestionIndex += 1
+        if currentQuestionIndex == questions.count {
+            currentQuestionIndex = 0
+        }
+        let question: String = questions[currentQuestionIndex]
+        questionLabel.text = question
+        answerLabel.text = "????"
+    }
+    
+    @IBAction func showAnswer(_ sender: UIButton) {
+        let answer: String = answers[currentQuestionIndex]
+        answerLabel.text = answer
+    }
+    
+}
+
